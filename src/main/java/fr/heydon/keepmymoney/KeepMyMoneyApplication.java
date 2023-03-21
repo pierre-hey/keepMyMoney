bb02abccd71788841de0f65446235e9946bfcf09
pierre-hey <83176715+pierre-hey@users.noreply.github.com>
2023-03-21 10:11:03 +0100

    init commit

---
+++ src/main/java/fr/heydon/keepmymoney/KeepMyMoneyApplication.java
@@ -0,0 +1,13 @@
+package fr.heydon.keepmymoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepMyMoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KeepMyMoneyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/heydon/keepmymoney/KeepMyMoneyApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.heydon.keepmymoney;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KeepMyMoneyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
